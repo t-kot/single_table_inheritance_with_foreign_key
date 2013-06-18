9f2a24e254b7c2b084c5272ac9c4ffb846f8f64d
kotohata <t.kotohata@gmail.com>
2013-06-18 23:49:09 +0900

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SingleTableInheritanceWithForeignKey::Application.config.secret_token = '6ef1c4a24f09eff47a2e8eea32e6b8394024d045aecb8cafadd9f40b28c84bf7da19ef2261f59130e7c80e5edaf5476f4808e89a67881f381747100050fd5690'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SingleTableInheritanceWithForeignKey::Application.config.session_store :cookie_store, key: '_single_table_inheritance_with_foreign_key_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SingleTableInheritanceWithForeignKey::Application.config.session_store :active_record_store
