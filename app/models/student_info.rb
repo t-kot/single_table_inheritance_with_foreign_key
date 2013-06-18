3c3abec3458fd5b72081ca14507175847bc39f50
kotohata <t.kotohata@gmail.com>
2013-06-19 00:38:04 +0900

    Modify

--- app/models/student_info.rb
+++ app/models/student_info.rb
@@ -1,2 +1,9 @@
 class StudentInfo < UserInfo
+  before_create :set_type
+  has_one :detail, class_name: 'Student', foreign_key: :info_id
+
+  private
+  def set_type
+    self.user_type_id = UserType.find_by_name('Student').id
+  end
 end
--- app/models/teacher_info.rb
+++ app/models/teacher_info.rb
@@ -1,2 +1,9 @@
 class TeacherInfo < UserInfo
+  before_create :set_type
+  has_one :detail, class_name: 'Teacher', foreign_key: :info_id
+
+  private
+  def set_type
+    self.user_type_id = UserType.find_by_name('Teacher').id
+  end
 end
